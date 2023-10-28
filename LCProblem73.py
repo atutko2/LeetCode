398f9ae5ea1d043b371144d8c0f3433bec1660b1
Adam Tutko <atutko@vols.utk.edu>
2023-10-28 14:58:25 -0400

    Update comments

--- LCProblem73.py
+++ LCProblem73.py
@@ -1,10 +1,11 @@
 """
+73. Set Matrix Zeroes
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
 You must do it in place.
 """
 # Passes - Beats 90% of Python3 users
-
+# Medium Difficulty Problem
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
